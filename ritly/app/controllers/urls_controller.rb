945f1dc29ecd7a2eede3cce26ad586b221d3979e
jeromeall <jeromeallouche01@gmail.com>
2014-01-25 15:50:24 -0800

    edit page added

--- ritly/app/controllers/urls_controller.rb
+++ ritly/app/controllers/urls_controller.rb
@@ -3,20 +3,49 @@ class UrlsController < ApplicationController
   	@url = Url.new
   end
 
+
+  # Create new row in db
   def create
   	@url = Url.create(url_params)
-  	@url['random_string'] = SecureRandom.urlsafe_base64(10)
-  	
+    if @url.random_string != ''
+          @url['random_string'] = @url.random_string
+        else
+          @url['random_string'] = SecureRandom.urlsafe_base64(10)
+    end
   	@url.save
   	redirect_to url_path(@url.id)
-
   end
 
+
+  # Redirect to URL
+  def transfer
+    random_string = params[:random_string]
+    url = Url.find_by_random_string(random_string)
+    
+    redirect_to "http://#{url.link}"
+  end
+
+
+  # Show page
   def show
-  	
   	@url = Url.find(params[:id])
+  end
+
+  def edit
+    @url = Url.find(params[:id])
+  end
+
+  def update
+     @url = Url.find(params[:id])
+ 
+  if @url.update(url_params)
+    redirect_to @url
+  else
+    render 'edit'
+  end
 
   end
+
 
   private
   def url_params
--- ritly/config/routes.rb
+++ ritly/config/routes.rb
@@ -1,11 +1,16 @@
 Ritly::Application.routes.draw do
 
+
+
   resources :urls
+	
+	get 'go/:random_string', to: "urls#transfer"
+	
+	get 'go/:random_string/preview', to: "urls#show"
 
   root to: "urls#index"
 
-  # post "/urls", to: "urls#create"  
-
   
+
 
 end
